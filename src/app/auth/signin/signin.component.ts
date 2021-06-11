5f1aeb9e295a6fa6e28f190deb392fe239320b9d
Ibrahim <i.benfaiala@gmail.com>
2021-06-12 00:46:05 +0200

    auth ok

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  signinForm: FormGroup;
+  // @ts-ignore
+  errorMessage: string;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+    // @ts-ignore
+    const email = this.signinForm.get('email').value;
+    // @ts-ignore
+    const password = this.signinForm.get('password').value;
+
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(['/news']).then(function(){
+
+        });
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import firebase from "firebase";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  isAuth: boolean;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if(user) {
+          this.isAuth = true;
+        } else {
+          this.isAuth = false;
+        }
+      }
+    );
+  }
+
+  onSignOut() {
+    this.authService.signOutUser();
   }
 
 }
